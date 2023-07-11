f13f91b41d4653d644d2d5a09963306be56f391f
Herick Silva <herick610@gmail.com>
2023-07-11 09:14:51 -0300

    Compartilhar momentos

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -2,6 +2,8 @@ import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { Moment } from 'src/app/Moments';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,7 +13,11 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent {
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
   async createHandler(moment: Moment) {
     const formData = new FormData();
@@ -29,7 +35,9 @@ export class NewMomentComponent {
     await this.momentService.createMoment(formData).subscribe();
 
     // exibir msg
+    this.messagesService.add('Momento adicionado com sucesso!');
 
     // redirect
+    this.router.navigate(['/']);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
   baseApiUrl: 'http://localhost:3333/',
 };
